b0725f54b6602c594bca99d5ddef5a54d9189046
Art Goldhammer <art.goldhammer@gmail.com>
2019-12-24 13:37:38 -0500

    more controls, css

--- src/cljs/lingua/views.cljs
+++ src/cljs/lingua/views.cljs
@@ -4,13 +4,38 @@
    [lingua.subs :as subs]
    ))
 
+(defn controls []
+  [:div.btn-row
+   [:div.src-url
+    [:label "Source URL: "]
+    [:input.src-url {:type "text"}]]
+   [:div.src-lang
+    [:label "Source Lang: "]
+    [:select
+     [:option.src-opt "English"]
+     [:option.src-opt "German"]
+     [:option.src-opt "Italian"]
+     [:option.src-opt "French"]]]
+   [:div.target-lang
+    [:label "Target Lang: "]
+    [:select
+     [:option.target-opt "German"]
+     [:option.target-opt "Italian"]
+     [:option.target-opt "French"]
+     [:option.target-opt "English"]]]
+   [:div.src-or-target
+    [:label "Display: "]
+    [:input.src-or-target {:type "button" :value  "source"}]]])
+
 (defn main-panel []
   (let [name (rf/subscribe [::subs/name])]
     [:div.container
-     [:div.btn-row
-      [:select
-       [:option "English"]
-       [:option "French"]]
-      [:input {:type "button" :value  "hi"}]]
-     [:div.tl
-      [:h1 "Hello from " @name]]]))
+     [controls]
+     [:div.from
+      [:textarea]]
+     [:div.to
+      [:textarea {:placeholder @name}]]
+     [:div.usertext
+      [:textarea]]
+     [:div.usertrans
+      [:textarea]]]))
