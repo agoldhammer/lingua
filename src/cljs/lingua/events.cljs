de2b31d564b78c48360ad862319b20b9b0fa3430
Art Goldhammer <art.goldhammer@gmail.com>
2019-12-26 12:04:38 -0500

    added ajax.core to events

--- src/cljs/lingua/events.cljs
+++ src/cljs/lingua/events.cljs
@@ -1,13 +1,24 @@
 (ns lingua.events
-  (:require-macros [cljs.core.async.macros :refer [go]])
+  #_(:require-macros [cljs.core.async.macros :refer [go]])
   (:require
    [re-frame.core :as rf]
    [lingua.db :as db]
-   [cljs-http.client :as http]
-   [cljs.core.async :refer [<!]]
+   #_[cljs-http.client :as http]
+   #_[cljs.core.async :refer [<! take!]]
+   [ajax.core :refer [GET #_POST]]
    [day8.re-frame.tracing :refer-macros [fn-traced #_defn-traced]]))
 
 (rf/reg-event-db
  ::initialize-db
  (fn-traced [_ _]
    db/default-db))
+
+(defonce api-url "http://localhost:8280/api/")
+
+(defn endpoint [id]
+  (str api-url id))
+
+(defn json-resp [url]
+  (GET (endpoint url)
+    :handler #(prn "Response:" %)
+    :error-handler #(prn "Error: " %)))
